35b944884eb53e27d4e102a3cb7a051a90ad4bb6
AntonioEspin <antoesdev91@gmail.com>
2024-02-02 17:36:20 -0500

    ADD localStorage

--- projects/02-tic tac toe/src/App.jsx
+++ projects/02-tic tac toe/src/App.jsx
@@ -7,9 +7,15 @@ import { WinnerModal } from "./components/WinnerModal"
 import { GameBoard } from "./components/GameBoard"
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
+  const [board, setBoard] = useState(()=> {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  })
 
-  const [turn, setTurn] = useState(TURNS.X)
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
 
   const [winner, setWinner] = useState(null)
 
@@ -17,6 +23,8 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   }
 
   const updateBoard = (index) => {
@@ -28,6 +36,10 @@ function App() {
     // cambiar turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    // Guardar en localStorage
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
 
     // verificar si hay ganador
     const newWinner = checkWinnerFrom(newBoard)
